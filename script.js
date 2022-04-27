bd2ec20cc9bd135cf4103ec9ba1ac31d987d4c9e
myMasterCoders <academyfit7@gmail.com>
2022-04-27 18:49:51 +0430

    character-counter

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const text=document.querySelector("#myText");
+const count=document.querySelector("#count");
+const maxLenght=280;
+
+text.addEventListener("keyup",(e)=>{
+  count.innerHTML= e.target.value.length
+  if(text.value.length <= maxLenght){
+    text.style.color="green"
+  }else{
+    text.style.color="red"
+    alert("کلمات وارد شده بیش از حد مجاز است")
+  }
+})
